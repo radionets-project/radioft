9084f673c27a4bec7077cb530bac398c32f1fd27
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2025-05-15 14:42:15 +0200

    new version

--- radioft/_version.py
+++ radioft/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev12+g112b9f4.d20250515'
-__version_tuple__ = version_tuple = (0, 1, 'dev12', 'g112b9f4.d20250515')
+__version__ = version = '0.1.dev18+g6da3fbf.d20250515'
+__version_tuple__ = version_tuple = (0, 1, 'dev18', 'g6da3fbf.d20250515')
